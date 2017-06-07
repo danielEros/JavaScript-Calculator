03f2d4a393505e7fe6b085985a76ecde7c151c63
Daniel Eros <drerosdaniel@gmail.com>
2017-06-07 10:51:05 +0200

    clean code in calculator.js

--- calculator.js
+++ calculator.js
@@ -18,20 +18,20 @@ function handleDigitClick(event){
 function handleOperationClick(event){
     if ($.isNumeric($('#display').text()[$('#display').text().length-1]) && !($('#display').text().includes('='))){
         $('#display').text($('#display').text() + ' ' + $(this).text() + ' ');
-         localStorage.setItem('floatAdded', 'false');
+        localStorage.setItem('floatAdded', 'false');
     }
 }
 
 function handleEqualsClick(event){
     if ($.isNumeric($('#display').text()[$('#display').text().length-1]) && !($('#display').text().includes('='))){
         $('#display').html($('#display').text() + ' = ' + '<strong>' + eval(($('#display').text())) + '</strong>');
-         localStorage.setItem('floatAdded', false);
+        localStorage.setItem('floatAdded', false);
     }
 }
 
 function handleClearClick(event){
     $('#display').text(0);
-     localStorage.setItem('floatAdded', false);
+    localStorage.setItem('floatAdded', false);
 }
 
 function handleFoatPointClick(event){
