9011004ea340a20dd6c0b7f4e4f1417312bdc530
Daniel Eros <drerosdaniel@gmail.com>
2017-06-07 22:42:04 +0200

    restructure handleClick function

--- calculator.js
+++ calculator.js
@@ -1,61 +1,49 @@
 function main(){
     localStorage.setItem('floatAdded', 'false');
-    $('.digit').on('click', handleDigitClick);
-    $('.operation').on('click', handleOperationClick);
-    $('#equals').on('click', handleEqualsClick);
-    $('#clear').on('click', handleClearClick);
-    $('#float_point').on('click', handleFoatPointClick);
-}
-
-function handleDigitClick(event){
-    displayedText = $('#display').text();
-    newText = $(this).text();
-    if (displayedText === '0' || displayedText.includes('=')){
-        $('#display').text(newText);
-    } else if (isShortNumber(displayedText)){
-        $('#display').text(displayedText + newText);
+    arrayOfQueries = ['.digit', '.operation', '#equals', '#clear', '#float_point'];
+    for(var i=0; i<arrayOfQueries.length; i++){
+        $(arrayOfQueries[i]).on('click', handleClick);
     }
 }
 
-function handleOperationClick(event){
+function handleClick(event){
     displayedText = $('#display').text();
     newText = $(this).text();
-    if ($.isNumeric(displayedText[displayedText.length-1]) && !(displayedText.includes('='))){
-        $('#display').text(displayedText + ' ' + newText + ' ');
-        localStorage.setItem('floatAdded', 'false');
-    }
-}
-
-function handleEqualsClick(event){
-    displayedText = $('#display').text();
-    newText = $(this).text();
-    if ($.isNumeric(displayedText[displayedText.length-1]) && !(displayedText.includes('='))){
-        $('#display').html(displayedText + ' = ' + '<strong>' + eval(displayedText) + '</strong>');
+    if ($.isNumeric(newText)){
+        if (displayedText === '0' || displayedText.includes('=')){
+            $('#display').text(newText);
+        } else if (isShortNumber(displayedText)){
+            $('#display').text(displayedText + newText);
+        }
+    } else if(['+', '-', '*', '/', '%', '='].indexOf(newText) !== -1){
+        if ($.isNumeric(displayedText[displayedText.length-1]) && !(displayedText.includes('='))){
+            if (['+', '-', '*', '/', '%'].indexOf(newText) !== -1){
+                $('#display').text(displayedText + ' ' + newText + ' ');
+            } else if(newText === '='){
+                $('#display').html(displayedText + ' = ' + '<strong>' + eval(displayedText) + '</strong>');
+            }
+            localStorage.setItem('floatAdded', 'false');
+        }
+    } else if (newText === 'C'){
+        $('#display').text(0);
         localStorage.setItem('floatAdded', false);
-    }
-}
-
-function handleClearClick(event){
-    $('#display').text(0);
-    localStorage.setItem('floatAdded', false);
-}
-
-function handleFoatPointClick(event){
-    displayedText = $('#display').text();
-    var floatAdded = eval(localStorage.getItem('floatAdded'));
-    if (!(floatAdded) && !(displayedText.includes('=')) && isShortNumber(displayedText)) {
-        $('#display').text(displayedText + '.');
-        localStorage.setItem('floatAdded', 'true');
+    } else if (newText === '.'){
+        var floatAdded = eval(localStorage.getItem('floatAdded'));
+        if (!(floatAdded) && !(displayedText.includes('=')) && isShortNumber(displayedText)) {
+            $('#display').text(displayedText + '.');
+            localStorage.setItem('floatAdded', 'true');
+        }
     }
 }
 
 function isShortNumber(numberToCheck){
+    const SHORTNUMBER = 12;
     if (numberToCheck.includes(' ')){
         var startIndex = numberToCheck.lastIndexOf(' ') + 1;
     } else {
         var startIndex = 0;
     }
-    return (numberToCheck.length - startIndex < 12);
+    return (numberToCheck.length - startIndex < SHORTNUMBER);
 }
 
 $(document).ready(main);
